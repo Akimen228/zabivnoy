f9ea9b6dfa02f81f7020ff17bbc68a8b6c7f6ae8
Akim <87607724+Akimen228@users.noreply.github.com>
2021-09-25 23:53:24 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+API_TOKEN = '1942292514:AAHoEhdYPVnoN6I4yN7lFyIgAFylgPOxwdk' # токен от тлеграм бота
+admin = 1499060992 # id админа, тобиж твой (взять тут - t.me/userinfobot)
+admin_name = '@CM0KE' # юзернейм админа, c @
+
+
+#слито в @smoke_software
