caa572714944a017343cb0ce5e6ed57a7a21696a
Akim <87607724+Akimen228@users.noreply.github.com>
2021-10-09 15:56:42 +0500

    config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-API_TOKEN = 'token' # токен от тлеграм бота
+API_TOKEN = '2023507634:AAE71f877tBmbuhqRG7q9IpbhBUWZ8_3JzA' # токен от тлеграм бота
 admin = 261821892 # id админа, тобиж твой (взять тут - t.me/userinfobot)
 admin_name = '@Akimen6' # юзернейм админа, c @
